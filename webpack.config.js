37960d67ad352780841d8b71712f26b296baca1b
bolshu <bolshu@MacBook-Pro-bolshu.local>
2020-05-31 17:26:17 +0300

    normalize css

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,42 @@
-const path = require("path");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const DIST_PATH = path.resolve(__dirname, "dist");
+const DIST_PATH = path.resolve(__dirname, 'dist');
 
 module.exports = {
   entry: {
-    index: ["./src/index.ts", "./src/style.css"]
+    index: ['./src/index.ts', './src/index.css'],
   },
   output: {
-    filename: "[name].js",
-    path: DIST_PATH
+    filename: '[name].js',
+    path: DIST_PATH,
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: "ts-loader",
-        exclude: /node_modules/
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/i,
-        loader: ExtractTextPlugin.extract("css-loader", "style-loader")
+        loader: ExtractTextPlugin.extract('css-loader', 'style-loader'),
       },
-    ]
+    ],
   },
   resolve: {
-    extensions: [".js", ".ts"]
+    extensions: ['.js', '.ts'],
   },
   devServer: {
     contentBase: DIST_PATH,
     compress: true,
-    port: 9000
+    port: 9000,
   },
   plugins: [
-    new ExtractTextPlugin("style.css"),
+    new ExtractTextPlugin('style.css'),
     new HtmlWebpackPlugin({
-      title: "Spreadsheet",
-    })
-  ]
+      title: 'Spreadsheet',
+    }),
+  ],
 };
